efdaa97eac764b76956437608e7bf71d04578d7a
nazliozcelik <nazliozelikk@icloud.com>
2022-06-08 09:02:34 +0300

    diving deep into restful web services

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into user values(1001, '2022-05-19', 'AB');
+insert into user values(1002, '2022-05-18', 'Jill');
+insert into user values(1003, '2022-05-17', 'Jam');
+insert into post values(11001, 'My First Post', 1001);
+insert into post values(11002, 'My Second Post', 1001);
+
+
+
+
